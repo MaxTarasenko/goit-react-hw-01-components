a49594355dd8489cdbfa4d089b53a49ddd8e2645
Maksym Tarasenko <max.tarasenko.slip@gmail.com>
2020-06-20 12:32:51 +0300

    Updates

---
+++ precache-manifest.6ed78c31037a814d41265c4d5d64528f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03a660302f933281c957f14165dba28",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a99a5f25de00884d6c8b",
+    "url": "/goit-react-hw-01-components/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "3b8a56bcdea609fb292f",
+    "url": "/goit-react-hw-01-components/static/css/main.0503e53d.chunk.css"
+  },
+  {
+    "revision": "a99a5f25de00884d6c8b",
+    "url": "/goit-react-hw-01-components/static/js/2.852757a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.852757a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8a56bcdea609fb292f",
+    "url": "/goit-react-hw-01-components/static/js/main.f3016201.chunk.js"
+  },
+  {
+    "revision": "923b5fb991aeea4ab369",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.928b5ac9.js"
+  }
+]);
